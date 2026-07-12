3d54a6d4d39383532f302c9e3c3109960a101f34
Sarthak Mandal <sarthakmandal17112002@gmail.com>
2026-07-12 20:54:13 +0530

    DSA Practice Problem

---
+++ SubsequencesWithSumUsingRecursion.java
@@ -0,0 +1,33 @@
+public class SubsequencesWithSumUsingRecursion {
+    static boolean solve(int[] arr, int index, int currentSum, int targetSum) {
+
+        if (index == arr.length) {
+            if (currentSum == targetSum) {
+                return true;
+            }
+            return false;
+        }
+
+        boolean ans1 = solve(arr, index + 1, currentSum + arr[index], targetSum);
+
+        boolean ans2 = solve(arr, index + 1, currentSum, targetSum);
+
+        if (ans1 == true || ans2 == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        // int[] arr = { 1, 2, 1 };
+        // int targetSum = 2;
+        int[] arr = {  5,6,7 };
+        int targetSum = 4;
+        int index = 0;
+        int currentSum = 0;
+
+        boolean finalAns = solve(arr, index, currentSum, targetSum);
+        System.out.println(finalAns);
+    }
+}
